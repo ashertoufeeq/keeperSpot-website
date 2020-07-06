d011b69630beff82b0c8bb5de7d0fe5e1f385a04
ashertoufeeq <ashertoufeeq@gmail.com>
2020-07-06 13:32:01 +0530

    Updates

---
+++ precache-manifest.057abafbcf00fb1f3400217088e55466.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70327a5583b9784b8c1911d831f31469",
+    "url": "/ashertoufeeq/keeperSpot-website/index.html"
+  },
+  {
+    "revision": "917fa242dee325bbd474",
+    "url": "/ashertoufeeq/keeperSpot-website/static/css/main.9ec202ba.chunk.css"
+  },
+  {
+    "revision": "16c918f6de332d59d3bf",
+    "url": "/ashertoufeeq/keeperSpot-website/static/js/2.a5c93eb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ashertoufeeq/keeperSpot-website/static/js/2.a5c93eb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "917fa242dee325bbd474",
+    "url": "/ashertoufeeq/keeperSpot-website/static/js/main.a031a16d.chunk.js"
+  },
+  {
+    "revision": "cbb8f869224576bf2073",
+    "url": "/ashertoufeeq/keeperSpot-website/static/js/runtime-main.df1f228e.js"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/ashertoufeeq/keeperSpot-website/static/media/Ionicons.b2e0fc82.ttf"
+  },
+  {
+    "revision": "64515cbb36c14ba8e2de59a854a588aa",
+    "url": "/ashertoufeeq/keeperSpot-website/static/media/ic_launcher-web.64515cbb.png"
+  }
+]);
