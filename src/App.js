7a745883a1b2be8e0d1d55a8a8ed8562135ae097
ashertoufeeq <ashertoufeeq@gmail.com>
2020-07-06 18:14:32 +0530

    Routes change

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import './index.css';
 import {Home} from './screens/home'
 import {Header} from "./components/header";
+import {BrowserRouter as Router, Route, Switch,} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header/>
     <div className={'body'}>
-      <Home/>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route path="/" exact render={(routeProps) => (<Home {...routeProps}/>)}/>
+        </Switch>
+      </Router>
     </div>
     </div>
   );
